63d1b9b698c1f92af0053337056456a66eec75f6
Pedro Hércules <46134763+PedroHercules0810@users.noreply.github.com>
2023-01-31 20:48:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+/*
+console.log("Hello, world!");
+
+console.log('Hello, world!');
+
+console.log(`Hello, world!`);
+
+console.log(2023);
+
+alert('7632847327846328764');
+*/
+
+/*
+var NomeDaVar = 10;
+
+let NomeDaLet = 10;
+
+//variaveis só podem ser declaradas com $ _ ou letra
+
+{
+    let NomeDaLet = 11;
+    console.log(NomeDaLet);
+}
+
+console.log(NomeDaLet);
+
+//const e let funcionam igual, ambos funcionam diferente se estão dentro ou fora do bloco, o var altera de acordo com o ultimo declarado.
+
+const NomeDaConst = 0;
+*/
+
+/*
+//tipos de variavel
+
+//undefined
+let var1 = undefined;
+let var2;
+
+//console.log(var1);
+
+//boolean
+let var3 = true;
+let var4 = false;
+
+//string
+let var5 = "Codigo";
+let var6 = 'codigu';
+let var7 = `code`;
+
+//number
+let var8 = 10;
+let var9 = 11.1;
+
+//dizer qual tipo da variavel
+console.log(typeof var5);
+
+//concatenar
+console.log('O tipo da variavel '+var5+" eh "+typeof(var5));
+
+console.log(`O tipo da variavel ${var8} eh ${typeof(var8)}`);
+*/
+
+// entrada de dados
+
+let nome = prompt('Digite seu nome:');
+
+let sobrenome = prompt('Digite seu sobrenome:');
+
+let idade = prompt('Digite sua idade:');
+
+id = 01;
+
+console.log(`Olah, ${nome} ${sobrenome}, voceh tem ${idade} anos e seu ID eh ${id}`);
